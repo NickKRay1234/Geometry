164b3f2f03dac87832d38974b13ed4d7763a31d1
NickKRay1234 <kolya.kalchuk@gmail.com>
2022-04-22 17:29:47 +0200

    - Created rectangle class;

--- Geometry/Form1.cs
+++ Geometry/Form1.cs
@@ -12,6 +12,7 @@ namespace Geometry
 
         Line line1;
         Line line2;
+        Rectangle rectangle1;
 
 
         public Form1()
@@ -28,6 +29,7 @@ namespace Geometry
             pen = new Pen(Color.Blue);
             line1 = new Line(100, 100, 200, 10);
             line2 = new Line(200, 10, 300, 100);
+            rectangle1 = new Rectangle(100, 100, 300, 300);
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -36,15 +38,20 @@ namespace Geometry
 
         private void Draw()
         {
-            graph.DrawRectangle(pen, 100, 100, 200, 200);
             Draw(line1);
             Draw(line2);
+            Draw(rectangle1);
             picture.Image = bmp;
         }
 
         private void Draw(Line line)
         {
             graph.DrawLine(pen, line.x1, line.y1, line.x2, line.y2);
+        }
+
+        private void Draw(Rectangle rectangle)
+        {
+            graph.DrawRectangle(pen, rectangle.x1, rectangle.y1, rectangle.width, rectangle.height);
         }
     }
 }
---
+++ Geometry/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    class Rectangle
+    {
+        public int x1, y1;
+        public int x2, y2;
+        public int width;
+        public int height;
+
+        public Rectangle(int x1, int y1, int x2, int y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+            this.width = this.x2 - this.x1;
+            this.height = this.y2 - this.y1;
+        }
+
+    }
+}
